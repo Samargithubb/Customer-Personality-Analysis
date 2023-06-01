f42c4a3197c751bd472a91f8f640154c82a52494
Samargithubb <100934815+Samargithubb@users.noreply.github.com>
2023-06-01 09:54:05 +0530

    data_ingestion.py code updated

--- Customer/components/data_ingestion.py
+++ Customer/components/data_ingestion.py
@@ -1,11 +1,9 @@
 import pandas as pd
 from Customer import utils
-from Customer.constants import training
 from Customer.exception import CustomerException
 from Customer.logger import logging
 from Customer.entity.config_entity import DataIngestionConfig
 from Customer.entity.artifacts_entity import DataIngestionArtifact
-from sklearn.model_selection import train_test_split
 import os
 import sys
 
@@ -16,6 +14,7 @@ class DataIngestion:
 
     def initiate_data_ingestion(self) -> DataIngestionArtifact:
         try:
+            logging.info(f"{'=' * 20}Data Insertion log started.{'=' * 20} ")
             logging.info(f"Exporting collection data as pandas dataframe")
 
             # Exporting collection data as pandas dataframe
@@ -24,34 +23,15 @@ class DataIngestion:
             logging.info(f"Save data in feature store having the shape: {df.shape} ")
             logging.info(f"Columns for the dataframe are : {df.columns} ")
 
-            # Save data in feature store
-            logging.info('Create feature store folder if not available')
-            feature_store_dir = os.path.dirname(self.data_ingestion_config.feature_store_file_path)
-            os.makedirs(feature_store_dir, exist_ok=True)
-            logging.info("Save df to feature store folder")
-
-            # Save df to feature store folder
-            df.to_csv(path_or_buf=self.data_ingestion_config.feature_store_file_path, index=False, header=True)
-            # training and test splitting.
-            logging.info('Splitting the data into train and test dataset')
-            train_df, test_df = train_test_split(df, test_size=training.DATA_INGESTION_TRAIN_TEST_SPLIT_RATION,
-                                                 random_state=1)
             logging.info("create dataset directory folder if not available")
-
             # create dataset directory folder if not available
             train_path_dir = os.path.dirname(self.data_ingestion_config.train_file_path)
             os.makedirs(name=train_path_dir, exist_ok=True)
-            logging.info('creating the dataset directory folder for test if not available')
-            test_path_dir = os.path.dirname(self.data_ingestion_config.test_file_path)
-            os.makedirs(name=test_path_dir, exist_ok=True)
-            train_df.to_csv(path_or_buf=self.data_ingestion_config.train_file_path, index=False, header=True)
-            test_df.to_csv(path_or_buf=self.data_ingestion_config.test_file_path, index=False, header=True)
+            df.to_csv(path_or_buf=self.data_ingestion_config.train_file_path, index=False, header=True, sep=',')
+            logging.info("Data Ingestion Completed")
             logging.info("Preparing the artifact")
-
             data_ingestion_artifact = DataIngestionArtifact(
-                feature_store_file=self.data_ingestion_config.feature_store_file_path,
-                train_file_path=self.data_ingestion_config.train_file_path,
-                test_file_path=self.data_ingestion_config.test_file_path)
+                train_file_path=self.data_ingestion_config.train_file_path)
             logging.info(f'Data Ingestion artifact : {data_ingestion_artifact} ')
             return data_ingestion_artifact
         except Exception as e:
